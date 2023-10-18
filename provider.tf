c019802eeb0d88391d86b7937948d7428f880c33
kevin-E-404 <edusandovla@gmail.com>
2023-10-18 18:36:29 +0200

    deploy

---
+++ provider.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+  }
+
+  
+  backend "azurerm" {
+    resource_group_name  = "terraform-github-actions-state"
+    storage_account_name = "terraformgithubactions"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
+ 
+
+data "azurerm_kubernetes_cluster" "credentials" {
+  name                = azurerm_kubernetes_cluster.demo.name
+  resource_group_name = azurerm_resource_group.demo.name
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.azurerm_kubernetes_cluster.credentials.kube_config.0.host
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.cluster_ca_certificate)
+
+  }
+}
